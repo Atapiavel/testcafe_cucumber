a30226ccd680b251b95e365d4866be73344a5e45
Mehrdad Hassani <mehrdad.hassani@scorpion.co>
2021-03-19 17:59:16 -0700

    Added more steps into the Messenger script

--- test/pages/MessengerPage _Old.js
+++ test/pages/MessengerPage _Old.js
@@ -9,6 +9,20 @@ class MessengerPage{
         this.peopleBtn = Selector('[title="comms-messenger.enum.people-tooltip"]');
         this.groupsBtn = Selector('[title="comms-messenger.enum.groups-tooltip"]');
         this.messagesBtn = Selector('[title="comms-messenger.enum.messages-tooltip"]');
+        this.addGroupsBtn = Selector('section:nth-child(2) > header > scorpion-icon');
+        this.addCoworkersBtn = Selector('section:nth-child(3) > header > scorpion-icon');
+        this.groupNameInput = Selector('[data-cy=input-wrapper]').withText('Name Your Group');
+        this.searchTeamMembers = Selector('[data-cy=member-search]');
+        this.selectMemberRadio = Selector('[aria-label="check"]');
+        this.createBtn = Selector('button').withText('Create');
+        this.kebobBtn = Selector('[data-cy=popup-menu-button]');
+        this.deleteGroupBtn = Selector('span').withText('Delete Group');
+        this.yesBtn = Selector('button').withText('Yes');
+        this.coworkerSearchInput = Selector('[data-cy=search-input]');
+        this.continueBtn = Selector('div').withText('Continue');
+        this.hideBtn = Selector('scorpion-menu-item > span');
+
+
 
     }
 }
