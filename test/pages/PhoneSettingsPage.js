74cf0aa5f3fa6f926eb0ec70fd7f2cc07b608e30
Mehrdad Hassani <mehrdad.hassani@scorpion.co>
2021-03-29 17:41:38 -0700

    Added more lines into the Phone Settings Script

--- test/pages/PhoneSettingsPage.js
+++ test/pages/PhoneSettingsPage.js
@@ -2,17 +2,22 @@ import {Selector,t} from 'testcafe';
 
 class PhoneSettingsPage{
     constructor(){
-        this.successmsg = Selector('strong').withText('Your Business Growth');
-        this.addbtn = Selector('[text="Add"]');
-        this.purchasenrs = Selector('[data-name="Purchase Numbers"]');
-        this.managenrs = Selector('[data-name="Manage Numbers"]');
-        this.routingconf = Selector('[data-name="Routing Configuration"]');
-        this.routinggrs = Selector('[data-name="Routing Groups"]');
-        this.routingKebob1row = Selector('tr:nth-child(1) > td.scorpion-cell.cdk-cell.icon-col.cdk-column-menus.scorpion-column-menus > scorpion-popup-menu > button > scorpion-icon')
-        this.searchbtn = Selector('[data-cy=button-search]');
-        this.filterbtn = Selector('[data-cy=trigger-button]');
-        this.cancelbtn = Selector('[data-cy=cancel-button]');
-        this.kebob1row = Selector('[data-cy=popup-menu-button]');
+        this.successMsg = Selector('strong').withText('Your Business Growth');
+        this.addBtn = Selector('[text="Add"]');
+        this.purchaseNrs = Selector('[data-name="Purchase Numbers"]');
+        this.manageNrs = Selector('[data-name="Manage Numbers"]');
+        this.routingConf = Selector('[data-name="Routing Configuration"]');
+        this.confTitle = Selector('[label="Configuration Title"]');
+        this.confDesc = Selector('[label="Configuration Description"]');
+        this.addRoutConfBtn = Selector('scorpion-modal-actions > scorpion-button:nth-child(1) > button');
+        this.deleteRoutConf = Selector('[content="Delete"]');
+        this.deleteConfirmBtn = Selector('[text="Delete"]');
+        this.routingGrs = Selector('[data-name="Routing Groups"]');
+        this.routingKebob1Row = Selector('tr:nth-child(1) [data-cy=popup-menu-button]')
+        this.searchBtn = Selector('[data-cy=button-search]');
+        this.filterBtn = Selector('[data-cy=trigger-button]');
+        this.cancelBtn = Selector('[data-cy=cancel-button]');
+        this.kebob1Row = Selector('[data-cy=popup-menu-button]');
       }
 }
 export default new PhoneSettingsPage();
