5bd01bf5f141b0716308028aa05fe675798ec495
Atapiavel <Atapiavel@github.com>
2021-04-23 12:46:13 -0500

    Invoice history refactor

---
+++ features/billing/preconditions.feature
@@ -0,0 +1,5 @@
+Feature: Tests precondition
+
+    @pre
+    Scenario: PRE_Invoice_filtering_by_year
+        When I execute the next query "PRE_Invoice_filtering_by_year"
--- pages/billing/invoice_history.pages.js
+++ pages/billing/invoice_history.pages.js
@@ -28,6 +28,28 @@ async function assert_columns(datatable) {
     }
 }
 
+async function assert_kebab_text(option_text, option){                  
+    await ActionsPage.click_element(kebab)
+    menu_option = "scorpion-menu-item:nth-of-type(" + option + ") > .full.nlf-between-center"
+    await ActionsPage.hover_element(menu_option)
+    const text = await select(menu_option).innerText
+    assert(text == option_text)
+    await ActionsPage.click_element(kebab)
+}
+
+async function assert_kebab_file_download(option_text, option){
+    await ActionsPage.click_element(kebab)
+    menu_option = "scorpion-menu-item:nth-of-type(3) > .full.nlf-between-center"
+    const text = await select(menu_option).innerText
+    assert(text == "Download")
+    await ActionsPage.hover_element(menu_option)
+    download_option = "[data-cy=nested-popup-menu-list] > scorpion-menu-item:nth-of-type(" + option + ") > .full.nlf-between-center"
+    const text_2 = await select(download_option).innerText
+    assert(text_2 == option_text)
+    await ActionsPage.hover_element(download_option)
+    await ActionsPage.click_element(kebab)
+}
+
 async function assert_kebab_option(option, datatable) {
     //counting the columns
     data = datatable.raw()
@@ -35,6 +57,8 @@ async function assert_kebab_option(option, datatable) {
     //counting the records
     const records = select('td:nth-of-type(2)');
     const records_count = await records.count
+    //i = records
+    //n = columns
     for (var i = 0; i < records_count; i++) {
         const record = "tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(2)"
         const text = await select(record).innerText
@@ -55,56 +79,19 @@ async function assert_kebab_option(option, datatable) {
                                 kebab = select("tbody .ng-star-inserted:nth-of-type(" + (i + 1) + ") scorpion-icon")
                                 await ActionsPage.hover_element(kebab)
                                 if (option == "send") {
-                                    await ActionsPage.click_element(kebab)
-                                    menu_option = "scorpion-menu-item:nth-of-type(1) > .full.nlf-between-center"
-                                    await ActionsPage.hover_element(menu_option)
-                                    const text = await select(menu_option).innerText
-                                    assert(text == "Send")
-                                    await ActionsPage.click_element(kebab)
+                                    await assert_kebab_text("Send","1")
                                 }
                                 if (option == "print") {
-                                    await ActionsPage.click_element(kebab)
-                                    menu_option = "scorpion-menu-item:nth-of-type(2) > .full.nlf-between-center"
-                                    await ActionsPage.hover_element(menu_option)
-                                    const text = await select(menu_option).innerText
-                                    assert(text == "Print")
-                                    await ActionsPage.click_element(kebab)
+                                    await assert_kebab_text("Print","2")
                                 }
                                 if (option == "download_PDF") {
-                                    await ActionsPage.click_element(kebab)
-                                    menu_option = "scorpion-menu-item:nth-of-type(3) > .full.nlf-between-center"
-                                    const text = await select(menu_option).innerText
-                                    assert(text == "Download")
-                                    await ActionsPage.hover_element(menu_option)
-                                    download_option = "[data-cy=nested-popup-menu-list] > scorpion-menu-item:nth-of-type(1) > .full.nlf-between-center"
-                                    const text_2 = await select(download_option).innerText
-                                    assert(text_2 == ".PDF")
-                                    await ActionsPage.hover_element(download_option)
-                                    await ActionsPage.click_element(kebab)
+                                    await assert_kebab_file_download(".PDF", "1")
                                 }
                                 if (option == "download_DOC") {
-                                    await ActionsPage.click_element(kebab)
-                                    menu_option = "scorpion-menu-item:nth-of-type(3) > .full.nlf-between-center"
-                                    const text = await select(menu_option).innerText
-                                    assert(text == "Download")
-                                    await ActionsPage.hover_element(menu_option)
-                                    download_option = "[data-cy=nested-popup-menu-list] > scorpion-menu-item:nth-of-type(2) > .full.nlf-between-center"
-                                    const text_2 = await select(download_option).innerText
-                                    assert(text_2 == ".DOC")
-                                    await ActionsPage.hover_element(download_option)
-                                    await ActionsPage.click_element(kebab)
+                                    await assert_kebab_file_download(".DOC", "2")
                                 }
                                 if (option == "download_CSV") {
-                                    await ActionsPage.click_element(kebab)
-                                    menu_option = "scorpion-menu-item:nth-of-type(3) > .full.nlf-between-center"
-                                    const text = await select(menu_option).innerText
-                                    assert(text == "Download")
-                                    await ActionsPage.hover_element(menu_option)
-                                    download_option = "[data-cy=nested-popup-menu-list] > scorpion-menu-item:nth-of-type(3) > .full.nlf-between-center"
-                                    const text_2 = await select(download_option).innerText
-                                    assert(text_2 == ".CSV")
-                                    await ActionsPage.hover_element(download_option)
-                                    await ActionsPage.click_element(kebab)
+                                    await assert_kebab_file_download(".CSV", "3")
                                 }
                             }
                         }
