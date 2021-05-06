1c575823f37c3a4a3689ee2f98315ee8cbc7ed57
Mehrdad Hassani <mehrdad.hassani@scorpion.co>
2021-05-05 17:21:05 -0700

    Updated chat.spec

--- features/step_definition/comms/chat.spec.js
+++ features/step_definition/comms/chat.spec.js
@@ -7,11 +7,34 @@ When('I wait for the Chat display', async function () {
   await testController.expect(ChatPageLocator.exists).ok;
 });
 
-When('I click Get Started button', async function () {
-  await waitFor(1000);
-  await ActionsPage.click_element(ChatPageLocator.GetStarted());
+When('I land on chat dashboard page', async function () {
+  await testController.expect(ChatPageLocator.exists).ok;
 });
 
-Then('I land on chat dashboard page', async function () {
+When('I click on the Agent Availability button', async function() {
+  await ActionsPage.click_element(ChatPageLocator.AgentAvailBtn());
+});
+
+Then('The Set Your Availability displays', async function() {
   await testController.expect(ChatPageLocator.exists).ok;
+});
+
+When('I click on toggle for Availability', async function() {
+  await ActionsPage.click_element(ChatPageLocator.AvailToggleBtn());
+});
+
+When('I click on toggle for Availability again', async function() {
+  await ActionsPage.click_element(ChatPageLocator.AvailToggleBtn());
+});
+
+When('I click on the Agent Availability button again', async function() {
+  await ActionsPage.click_element(ChatPageLocator.AgentAvailBtn());
+});
+
+When('I click on Mine button', async function() {
+  await ActionsPage.click_element(ChatPageLocator.MineBtn());
+});
+
+When('I click on All button', async function() {
+  await ActionsPage.click_element(ChatPageLocator.AllBtn());
 });
