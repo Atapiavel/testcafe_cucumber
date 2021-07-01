4062bc46a521c4bdecb561b3bfb2a430893fecf2
Mehrdad Hassani <mehrdad.hassani@scorpion.co>
2021-06-30 17:43:27 -0700

    Updated the main and requests for comms

--- api/comms/main.js
+++ api/comms/main.js
@@ -1,7 +1,7 @@
 const Requests = require("./requests.js");
 const fetch = require("node-fetch");
-var bearer = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImU1Mzg0NTE3LTdlM2ItNDQxYi1hMjczLTk0MDcxODU1ODEwNSIsInR5cCI6IkpXVCJ9.eyJzdWlkIjoiRDAxQ0I4OEJGMEI4ODY0QjlDRjFBMzBGRDg2MzNEM0MiLCJleHAiOjE2MjM0NTI2MDgsIm5iZiI6IjE2MjM0NDkwMDgiLCJlbnYiOiJpbnRlZ3JhdGlvbi10eCIsImlkZW50aXR5IjoiZWFiYWQzNGItMWVlNi00NzdjLWFiOGYtMmQwNTJkYmE4ZGQ5IiwidXNlcklkIjoxMzM2OCwidXNlckd1aWQiOiIwYWU1YzljZC1kZTZmLTQ0OWYtMDM4Zi0wNzhlOTBjMDUxMGYiLCJuYW1lIjoiQXJ0dXJvIFRhcGlhIFZlbGFzY28iLCJlbWFpbCI6ImFydHVyby50YXBhaWFAc2NvcnBpb24uY28iLCJhY2NvdW50IjoiYjk2YmU5ZDAtNmNhYi05OTQ2LWE0NTgtMmE1MWVlOTE5OTQ4Iiwic2NvcnBpb25Vc2VyIjpmYWxzZSwibG9jYWxVc2VyIjpmYWxzZSwiaW1wZXJzb25hdGVkIjpmYWxzZSwibXVsdGlUZW5hbnQiOmZhbHNlLCJjbGllbnRJZCI6NDE5NywiY2xpZW50R3VpZCI6ImI5NmJlOWQwLTZjYWItOTk0Ni1hNDU4LTJhNTFlZTkxOTk0OCIsIm9yaWdpbmFsQ2xpZW50SWQiOjQxOTcsIm9yaWdpbmFsQ2xpZW50R3VpZCI6ImI5NmJlOWQwLTZjYWItOTk0Ni1hNDU4LTJhNTFlZTkxOTk0OCIsInN5c3RlbVJvbGVzIjoiMCIsImFjY291bnRSb2xlcyI6IjAiLCJwZXJtaXNzaW9ucyI6IlsyNDMxODg0NiwyNjIxNDQsMSw4MjExODk5MjAsMTY3ODExOTIsMCwwLDAsMCwwLDAsMCwwLDAsMCwwXSIsImFwaWtleSI6ZmFsc2UsImVuYWJsZUFwaUxvZyI6ZmFsc2UsImhpcGFhIjpmYWxzZSwiaXNzIjoiaWRlbnRpdHkuc2NvcnBpb24uY28iLCJhdWQiOiJ1c2VycyJ9.KJ0ss8p0z4axGWZKHk_pc5UxnTLJP3mwxXp6cxvwwCK1ifqYmeQ6kht3pm77_WvcWjUmHcjxxJ00GKkmdamJ4fvz7Lh-Il-RtVxoVBZy7k55kftG3B9afwj0tHMBx-_RpzmAn390hLvaSN4Qn5dcC0jlPzBHHTvCOq96N4tWtPvmbhN37J_a7ro04-Ta2Bq7k16yJPLNEfHjR6mSnjOcaoF0ltRrEAH7S_AF2a1a5zATgRnc3JZEECQp_v3MQiAjc9PCpuCM1LZYe610M1jNpusjC53jBxabgx20qRIVkAqCBS82hp2mq1kxWzi2LqKkqxhTFOEI9Sx7Hih3UKtGvoUfweF4pXX7sgAqK4JaucnTgwxYvDhF5hAk4Hn_Z_qKBYnljDnOA7RXwjAFb0NKnBrHxNVVgCFouvGU4wUvTMEOtyuZdWEzKFl1VKX2y4Bg99pGTtFYssi0Ez2FCArMx6oLNvbfPcaALW97lNg56F1r-gRC9AVQaaxxJTfOemIvOe_4c-FMEKfkbO_-9m1ZzogId6xuZVXQbdIKX3_KjmiSMOXitfRZb8u4WX8cU_OnL5sA3RVW_H_I5r4yz79EGuNH5uBWVbw-iW9pwcgtv_pLDvteom05MptbEdYmVVVvUAS0dufN0BmKaJjOSzmb-sHc3waAwgVbWBoEx_fySIo"
-var url = "https://integration.scorpion.co/csx/billing/graphql"
+var bearer = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImU1Mzg0NTE3LTdlM2ItNDQxYi1hMjczLTk0MDcxODU1ODEwNSIsInR5cCI6IkpXVCJ9.eyJzdWlkIjoiQzEyQTZGNENGNjQ3QkQ0MDhBMThFRTc2RDc1RkJGREUiLCJleHAiOjE2MjUxMDE5MjksIm5iZiI6IjE2MjUwOTgzMjkiLCJlbnYiOiJpbnRlZ3JhdGlvbi10eCIsImlkZW50aXR5IjoiMDAwOTM5MTUtYTZhMC00ZjY3LTgzZmEtY2E4YmQxMDkwOWIzIiwidXNlcklkIjoxNDUzOCwidXNlckd1aWQiOiJhNTkwYjZmOC1lMGI3LWZhZGYtOWRkZS0yOTNmMGE2ZWIzZDciLCJuYW1lIjoiQ3VzdG9tZXIgU2VydmljZSBSZXAiLCJlbWFpbCI6ImNvbW1jZW50ZXJAc2NvcnBpb24uY28iLCJhY2NvdW50IjoiYjk2YmU5ZDAtNmNhYi05OTQ2LWE0NTgtMmE1MWVlOTE5OTQ4Iiwic2NvcnBpb25Vc2VyIjp0cnVlLCJsb2NhbFVzZXIiOmZhbHNlLCJpbXBlcnNvbmF0ZWQiOmZhbHNlLCJtdWx0aVRlbmFudCI6dHJ1ZSwiY2xpZW50SWQiOjQxOTcsImNsaWVudEd1aWQiOiJiOTZiZTlkMC02Y2FiLTk5NDYtYTQ1OC0yYTUxZWU5MTk5NDgiLCJvcmlnaW5hbENsaWVudElkIjo0MTk3LCJvcmlnaW5hbENsaWVudEd1aWQiOiJiOTZiZTlkMC02Y2FiLTk5NDYtYTQ1OC0yYTUxZWU5MTk5NDgiLCJzeXN0ZW1Sb2xlcyI6IjAiLCJhY2NvdW50Um9sZXMiOiIwIiwicGVybWlzc2lvbnMiOiJbMjQzMTg4NDYsNjU1MzYwMCwxLDgyMTE4OTkyMCwzMzU0MjAyNCwwLDAsMCwwLDAsMCwwLDAsMCwwLDBdIiwiYXBpa2V5IjpmYWxzZSwiZW5hYmxlQXBpTG9nIjpmYWxzZSwiaGlwYWEiOmZhbHNlLCJpc3MiOiJpZGVudGl0eS5zY29ycGlvbi5jbyIsImF1ZCI6InVzZXJzIn0.Q7dIpIi2eRyVHr_GN3YfvXLpN6OdMGJVOz_-3g7zEwU3BbONm1P9SBBvCJodk5lryqHHf-FLqRkiTvGRK4iDw3oa9OG3azjxwgd56kLzJup-0h6Pz7EpC8_DLS3KKCtKxCz9Bx9AzEL5MEL5DwIdX_vcTEu8qUQceBAhYnT7d3E8Pnq-K1XgYKzSL_NXHYEoqZSU_4husdgosTcVxii8UBetHbUnXD1dzqQOAbaWSHOK-S4Ta4RwFiffOidTrVcFmpzAemnR0h5KWOnWfbO5nmKZQdT9_AffL1Z5baFnBJLMLsF2Xb_-cHf9UVtxRZVe-_NmFPrvExeLd8xaJvbsFUJAEh1CWoCHSDYtYohwpM1_8r4nJaF0CXqEfohLSw0ovo2hX0NK7jndT0t47gYCGZw7KhXhU2VSXRTx4Zk77kHiDFK4ks2EQLwldDzd2lQdOLHu-XIBtXj1dzwiPKSOdFemfBXHoEdpXQe0C5jmHDM5lGMmzp2lRy4r6N-ZWPS0xQwa4lpiHr6I10jumdbS8lZNm2S-S1AK4NmXkmQtigYb4cDQpuZzrBj5r5KNfGBi0MftjBe-jZi5OLGhepw-QjGn2WP4K4t0-7QevAqbpIFRbi2wddrZ8mwS6REabaG-i2cGi_58qA5lhRKsVnvG99Sfiq6aMUzsBNZrgvMXElA"
+var url = "https://integration.scorpion.co/communicationsplatform/commscenter/graphql"
 const headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
@@ -20,13 +20,13 @@ function execute_request(url, headers, request) {
                 .then(r => r.json())
                 .then(data => {
                         console.log(data)
-                        console.log(data.data)
-                        console.log(data.data.getInvoiceList)
-                        console.log(data.data.getInvoiceList.items)
-                        console.log(data.data.getInvoiceList.items[0])
-                        console.log(data.data.getInvoiceList.items[0].invoiceId)
+                        // console.log(data.data)
+                        // console.log(data.data.communicationSearch)
+                        // console.log(data.data.communicationSearch.items)
+                        // console.log(data.data.communicationSearch.items[0])
+                        // console.log(data.data.communicationSearch.items[0].communicationID)
                 })
 
 }
 
-execute_request(url, headers, Requests.getInvoiceList(100))
+execute_request(url, headers, Requests.getCommunicationList(100))
