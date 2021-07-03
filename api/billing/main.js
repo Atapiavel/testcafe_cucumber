8b4fc355809a3f589734f9df20f66c9be1f6cbc4
Atapiavel <Atapiavel@github.com>
2021-07-02 19:36:22 -0500

    Last changes

--- api/billing/main.js
+++ api/billing/main.js
@@ -1,8 +1,13 @@
 const Requests = require("./requests");
-const ActionsPage = require("../../pages/actions.pages")
 const fetch = require("node-fetch");
 const fs = require('fs');
-const billing_url = 'https://integration.scorpion.co/csx/billing/graphql'
+
+var bearer = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImU1Mzg0NTE3LTdlM2ItNDQxYi1hMjczLTk0MDcxODU1ODEwNSIsInR5cCI6IkpXVCJ9.eyJzdWlkIjoiNzA5NDY2Q0U2NDQ1QkE0OUFBNTMzRUE3REVEOTIxOTgiLCJleHAiOjE2MjUyNzQ3MzQsIm5iZiI6IjE2MjUyNzExMzQiLCJlbnYiOiJpbnRlZ3JhdGlvbi10eCIsImlkZW50aXR5IjoiNGJkYTJhZTItMDM1Ny00Y2JmLWI1NTAtNmJhMDY2NWMzNWE1IiwidXNlcklkIjoxNDU4MCwidXNlckd1aWQiOiJmYzM5MWVkMC04ZmU1LWE1YmYtOTlhMy05NTE1MGFmMjhmM2IiLCJuYW1lIjoiQmlsbGluZyBUZWFtIiwiZW1haWwiOiJ0aGViaWxsaW5ndGVhbUBzY29ycGlvbi5jbyIsImFjY291bnQiOiJiOTZiZTlkMC02Y2FiLTk5NDYtYTQ1OC0yYTUxZWU5MTk5NDgiLCJzY29ycGlvblVzZXIiOmZhbHNlLCJsb2NhbFVzZXIiOnRydWUsImltcGVyc29uYXRlZCI6ZmFsc2UsIm11bHRpVGVuYW50IjpmYWxzZSwiY2xpZW50SWQiOjQxOTcsImNsaWVudEd1aWQiOiJiOTZiZTlkMC02Y2FiLTk5NDYtYTQ1OC0yYTUxZWU5MTk5NDgiLCJvcmlnaW5hbENsaWVudElkIjo0MTk3LCJvcmlnaW5hbENsaWVudEd1aWQiOiJiOTZiZTlkMC02Y2FiLTk5NDYtYTQ1OC0yYTUxZWU5MTk5NDgiLCJzeXN0ZW1Sb2xlcyI6IjAiLCJhY2NvdW50Um9sZXMiOiIwIiwicGVybWlzc2lvbnMiOiJbMjQzMTg4NDYsMjYyMTQ0LDEsODIxMTg5OTIwLDE2NzgxMTkyLDAsMCwwLDAsMCwwLDAsMCwwLDAsMF0iLCJhcGlrZXkiOmZhbHNlLCJlbmFibGVBcGlMb2ciOmZhbHNlLCJoaXBhYSI6ZmFsc2UsImlzcyI6ImlkZW50aXR5LnNjb3JwaW9uLmNvIiwiYXVkIjoidXNlcnMifQ.E2HFXPmipmdYp-7BksurcqIMlb3IRkOz7fUYq9d6M5Nyv0aQxjBqau0QP0ufrhutKQFbUNNCa1xHWN0FBWuGIz-fdW8bKkTvY0QVyEyb1N_fy1qbfKpr-0MNKVK3Ofb9NkN76kONQ8yl9Cz0vVrtmqekET9s54jV6jQgIX4Dh7giK_cNW9GctSN5jhPYdLsF8UK31p_WxIp1c5koDFFCTKgKwFxqmCW7NxbivzyFpv-crJxQ0DEt0Y9OmSbuTgF-0hQUx2FtvkhjV1Nf4IHe6F09a5sbB96GvtVZCO-PSZqVTdO4rWF0OB32V_NhhTbqHxsptFZu9uYBz081JgZSrlEeM9K-k3M1ajT3SWqDmeBqwXWltQAKEhlkSfGD77PUqeVbK0yeCUgSeqxEYCwglwR1VW7rRCX8zI4mYC88PwIHhyAIy5vbxUY9hbGTpksZKHU0XlfTjhsseuRvdZv4A3M8KF_d6bsrBY10YrkBGtAKtUOTYKcElJ-K4HuBq2rJeEtipOmyjMFxhgwLwu4iXUx-Xj1eYsbG_UhGiAJS8yzLjQhvYKARhhEXbd3HEfD8VfSFCldJwUSvDXqTs-sGOqYOnH4VfbeyNfpsVVCGwmSuA67_W3xip45v5Yb2cbJTmvyC7hHkjzNHFebYOxv2RtB0YPfWWH7PCLjFkD-N0PM"
+const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Bearer ' + bearer
+}
 
 var act_date = new Date();
 var year = act_date.getFullYear();
@@ -73,7 +78,6 @@ function getAccountMonies(url, headers, serviceLineId) {
 }
 
 function getInvoiceHistoryData(url, headers) {
-        console.log(headers)
         var invoice_arr = []
         var z = 0
         fetch(url, {
@@ -84,8 +88,8 @@ function getInvoiceHistoryData(url, headers) {
                 })
         })
                 .then(r => r.json())
-                .then(data => {
-                        console.log(data)
+                .then(function () {
+                        // console.log(data)
                         var number_of_invoices = data.data.getInvoiceList.items.length
                         for (var n = 0; n < number_of_invoices; n++) {
                                 let due_date = new Date(data.data.getInvoiceList.items[n].dueDate);
@@ -169,8 +173,8 @@ function fetchAuthToken(base_url, password, username) {
                         })
                                 .then(r => r.json())
                                 .then(data => {
-                                        console.log(data)
                                         var token = String(data.id_token)
+                                        console.log(data)
                                         // fs.unlinkSync('bearer.txt');
                                         // fs.appendFileSync("bearer.txt", token, "UTF-8", { 'flags': 'a+' });
                                         fs.unlinkSync('./api/billing/bearer.txt');
@@ -193,7 +197,6 @@ function getCurrentUser(base_url, headers) {
 }
 
 function logoff(base_url, headers) {
-        console.log(headers)
         const url = base_url + "/platform/identity/v1/api/oauth2logoff/logoff"
         fetch(url, {
                 method: 'POST',
@@ -207,9 +210,10 @@ function logoff(base_url, headers) {
 }
 
 // Playground
-
+const billing_url = "https://integration.scorpion.co/csx/billing/graphql"
 // logoff("https://integration.scorpion.co", headers)
-// getAmountDue(billing_url)
+getAmountDue(billing_url, headers)
+getAccountMonies(billing_url, headers,1)
 // getInvoiceHistoryData(billing_url, headers)
 // fs.unlinkSync('../../bearer.txt');
 // fetchAuthToken("https://integration.scorpion.co", "Billing1234!!", "thebillingteam@scorpion.co")
